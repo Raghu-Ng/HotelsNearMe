c1e941142a2fe00207cdd32f87b183e534dd9187
Raghava Reddy N <raghau.20003@gmail.com>
2023-11-21 19:07:05 +0530

    Committing changes to src/API/index.js

--- src/API/index.js
+++ src/API/index.js
@@ -13,7 +13,7 @@ export const getPlacesData = async (type, sw, ne) => {
           tr_longitude: ne.lng,
         },
         headers: {
-          'X-RapidAPI-Key': 'f6233d2ac4msheb6def50add92f9p11dc9fjsn34d57eb63a3c',
+          'X-RapidAPI-Key': '34ac606514mshdfee98baea4fc17p1e0586jsn3e7f7bf535a9',
     'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
         }});
 
